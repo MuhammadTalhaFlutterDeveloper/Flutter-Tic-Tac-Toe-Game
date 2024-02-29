9137cb236189768f067940fabe46232e17e2d092
MuhammadTalhaFlutter <86160135+MuhammadTalhaFlutterDeveloper@users.noreply.github.com>
2024-02-29 09:05:19 +0500

    initial
    initial commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+ 
+  static Color backgroundColor = const Color(0xff12486B);
+  static Color kPinkColor = const Color(0xff720455);
+  static Color kGreenMore = const Color(0xff005B41);
+  static Color kTransparent = Colors.transparent;
+  static Color kWhite = Colors.white;
+  static Color kBlack = Colors.black;
+  static Color kRed = Colors.red;
+  static Color kRedA = Colors.redAccent;
+  static Color kPink = Colors.pink;
+  static Color kPinkA = Colors.pinkAccent;
+  static Color kBlue = Colors.blue;
+  static Color kBlueA = Colors.blueAccent;
+  static Color kLightBlue = Colors.lightBlue;
+  static Color kLightBlueA = Colors.lightBlueAccent;
+  static Color kGreen = Colors.green;
+  static Color kGreenA = Colors.greenAccent;
+  static Color kLightGreen = Colors.lightGreen;
+  static Color kLightGreenA = Colors.lightGreenAccent;
+  static Color kLime = Colors.lime;
+  static Color kLimeA = Colors.limeAccent;
+  static Color kGrey = Colors.grey;
+  static Color kBlueGrey = Colors.blueGrey;
+  static Color kPurple = Colors.purple;
+  static Color kPurpleA = Colors.purpleAccent;
+  static Color kDeepPurple = Colors.deepPurple;
+  static Color kDeepPurpleA = Colors.deepPurpleAccent;
+  static Color kIndigo = Colors.indigo;
+  static Color kIndigoA = Colors.indigoAccent;
+  static Color kYellow = Colors.yellow;
+  static Color kYellowA = Colors.yellowAccent;
+  static Color kOrange = Colors.orange;
+  static Color kOrangeA = Colors.orangeAccent;
+  static Color kDeepOrange = Colors.deepOrange;
+  static Color kDeepOrangeA = Colors.deepOrangeAccent;
+  static Color kAmber = Colors.amber;
+  static Color kAmberA = Colors.amberAccent;
+  static Color kCyan = Colors.cyan;
+  static Color kCyanA = Colors.cyanAccent;
+  static Color kTeal = Colors.teal;
+  static Color kTealA = Colors.tealAccent;
+  static Color kBrown = Colors.brown;
+}
